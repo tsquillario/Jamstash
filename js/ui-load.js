e6b8e131fd8d454e7852737ca17b958ba53a46e0
Trevor Squillario <tsquillario@gmail.com>
2012-04-03 16:04:00 -0400

    1.9.6 misc bug fix

--- js/ui-load.js
+++ js/ui-load.js
@@ -26,13 +26,13 @@ $(window).resize(function () {
 });
 function resizeContent() {
     var screenwidth = $(window).width();
-    $('.tabcontent').css({ 'height': (($(window).height() - 112)) + 'px' });
+    $('.tabcontent').css({ 'height': (($(window).height() - 109)) + 'px' });
     $('.smsection').css({ 'height': (($(window).height() - 152)) + 'px' });
     var smheight = $('.smsection').height();
     var smwidth = $('.smsection').width();
     $('#BottomContainer').css({ 'top': smheight + 35 + 'px' });
     if ($.cookie('sidebar')) {
-        var tabwidth = $(window).width() - 268;
+        var tabwidth = $(window).width() - 264;
         if (tabwidth >= 700) {
             $('.tabcontent').css({ 'width': tabwidth + 'px' });
         }
@@ -40,7 +40,7 @@ function resizeContent() {
         $('#SideBar').css({ 'height': (sbheight + 108) + 'px' });
         $('#ChatMsgs').css({ 'height': (sbheight - 166) + 'px' });
     } else {
-        var tabwidth = $(window).width() - 61;
+        var tabwidth = $(window).width() - 58;
         if (tabwidth >= 700) {
             $('.tabcontent').css({ 'width': tabwidth + 'px' });
         }
