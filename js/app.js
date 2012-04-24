990dcc47762749682c7a2ed1f942bcd1b5ac7be1
Javier Aguirre <contacto@javaguirre.net>
2012-04-24 20:37:23 +0200

    Changed methods from app to api.js

--- js/app.js
+++ js/app.js
@@ -62,57 +62,5 @@ function loadTabContent(tab) {
     }
 }
 
-var updaterNowPlaying;
-var updaterNowPlayingData;
-function updateNowPlaying() {
-    updaterNowPlaying = $.periodic({ period: 4000, decay: 1.5, max_period: 1800000 }, function () {
-        $.ajax({
-            periodic: this,
-            url: baseURL + '/getNowPlaying.view?u=' + username + '&p=' + passwordenc + '&v=' + version + '&c=' + applicationName + '&f=jsonp',
-            method: 'GET',
-            dataType: 'jsonp',
-            timeout: 10000,
-            beforeSend: function (req) {
-                req.setRequestHeader('Authorization', auth);
-            },
-            success: function (data) {
-                if (data["subsonic-response"].nowPlaying.entry === undefined) {
-                    this.periodic.increment();
-                    $("#NowPlayingList").empty();
-                    var chathtml = '<div class=\"msg\">';
-                    chathtml += '<span class=\"user\">Nothing :(</span></br>';
-                    chathtml += '</div>';
-                    $(chathtml).appendTo("#NowPlayingList");
-                } else if (updaterNowPlayingData === $.param(data)) {
-                    this.periodic.increment();
-                } else {
-                    $("#NowPlayingList").empty();
-                    var msgs = [];
-                    if (data["subsonic-response"].nowPlaying.entry.length > 0) {
-                        msgs = data["subsonic-response"].nowPlaying.entry;
-                    } else {
-                        msgs[0] = data["subsonic-response"].nowPlaying.entry;
-                    }
-                    this.periodic.reset();
-                    var sorted = msgs.sort(function (a, b) {
-                        return a.minutesAgo - b.minutesAgo;
-                    });
-                    $.each(sorted, function (i, msg) {
-                        var chathtml = '<div class=\"msg\">';
-                        chathtml += '<span class=\"user\">' + msg.username + '</span></br>';
-                        chathtml += '<span class=\"artist\">' + msg.artist + '</span> - ';
-                        chathtml += '<span class=\"title\">' + msg.title + '</span>';
-                        chathtml += '</div>';
-                        $(chathtml).appendTo("#NowPlayingList");
-                    });
-                    updaterNowPlayingData = $.param(data);
-                }
-            }
-        });
-    });
-}
-function stopUpdateNowPlaying() {
-    updaterNowPlaying.cancel();
-}
 
 
