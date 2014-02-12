bb7d018de4ab3b9d0b801a2a76b13aad49b559ab
Emory P <emory.petermann@axis80.com>
2014-02-12 01:18:36 -0500

    js-beautify all the controllers, services and directives (and jshint)

--- js/controllers/podcasts.js
+++ js/controllers/podcasts.js
@@ -1,112 +1,142 @@
 ﻿JamStash.controller('PodcastCtrl',
-function PodcastCtrl($scope, $rootScope, $location, utils, globals, model, notifications) {
-    //$("#left-component").layout($scope.layoutTwoCol);
+    function PodcastCtrl($scope, $rootScope, $location, utils, globals, model, notifications) {
 
-    $rootScope.song = [];
-    $scope.podcasts = [];
-    $scope.selectedPodcast;
-    $scope.getPodcasts = function (refresh) {
-        if (globals.settings.Debug) { console.log("LOAD PODCASTS"); }
-        $.ajax({
-            url: globals.BaseURL() + '/getPodcasts.view?' + globals.BaseParams(),
-            method: 'GET',
-            dataType: globals.settings.Protocol,
-            timeout: globals.settings.Timeout,
-            success: function (data) {
-                if (data["subsonic-response"].podcasts.channel !== undefined) {
-                    var items = [];
-                    if (data["subsonic-response"].podcasts.channel.length > 0) {
-                        items = data["subsonic-response"].podcasts.channel;
-                    } else {
-                        items[0] = data["subsonic-response"].podcasts.channel;
-                    }
-                    $scope.podcasts = items;
-                    $scope.$apply();
-                }
-            }
-        });
-    }
-    $scope.getPodcast = function (id, action) {
-        $scope.selectedPodcast = id;
-        var map = function (data) {
-            var song = data;
-            var url, track, rating, starred, contenttype, suffix, description;
-            var specs = '', coverartthumb = '', coverartfull = '';
-            if (typeof song.coverArt != 'undefined') {
-                coverartthumb = globals.BaseURL() + '/getCoverArt.view?' + globals.BaseParams() + '&size=60&id=' + song.coverArt;
-                coverartfull = globals.BaseURL() + '/getCoverArt.view?' + globals.BaseParams() + '&id=' + song.coverArt;
-            }
-            if (typeof song.description == 'undefined') { description = ''; } else { description = song.description; }
-            if (typeof song.track == 'undefined') { track = '&nbsp;'; } else { track = song.track; }
-            if (typeof song.starred !== 'undefined') { starred = true; } else { starred = false; }
-            if (song.bitRate !== undefined) { specs += song.bitRate + ' Kbps'; }
-            if (song.transcodedSuffix !== undefined) { specs += ', transcoding:' + song.suffix + ' > ' + song.transcodedSuffix; } else { specs += ', ' + song.suffix; }
-            if (song.transcodedSuffix !== undefined) { suffix = song.transcodedSuffix; } else { suffix = song.suffix; }
-            if (suffix == 'ogg') { suffix = 'oga'; }
-            var salt = Math.floor(Math.random() * 100000);
-            url = globals.BaseURL() + '/stream.view?' + globals.BaseParams() + '&id=' + song.streamId + '&salt=' + salt;
-            return new model.Song(song.streamId, song.parent, track, song.title, song.artist, song.artistId, song.album, song.albumId, coverartthumb, coverartfull, song.duration, song.userRating, starred, suffix, specs, url, 0, description);
-        }
-        $.ajax({
-            url: globals.BaseURL() + '/getPodcasts.view?' + globals.BaseParams(),
-            method: 'GET',
-            dataType: globals.settings.Protocol,
-            timeout: globals.settings.Timeout,
-            success: function (data) {
-                if (data["subsonic-response"].podcasts.channel !== undefined) {
-                    var podcasts = [];
-                    if (data["subsonic-response"].podcasts.channel.length > 0) {
-                        podcasts = data["subsonic-response"].podcasts.channel;
-                    } else {
-                        podcasts[0] = data["subsonic-response"].podcasts.channel;
-                    }
-                    var items = [];
-                    $.each(podcasts, function (i, item) {
-                        if (item.id == id) {
-                            items = item.episode;
-                        }
-                    });
+        $rootScope.song = [];
+        $scope.podcasts = [];
+        $scope.selectedPodcast = null;
 
-                    if (typeof items != 'undefined') {
-                        if (action == 'add') {
-                            angular.forEach(items, function (item, key) {
-                                if (item.status != "skipped") {
-                                    $rootScope.queue.push(map(item));
-                                }
-                            });
-                            $scope.$apply();
-                            $rootScope.showQueue();
-                            notifications.updateMessage(items.length + ' Song(s) Added to Queue', true);
-                        } else if (action == 'play') {
-                            $rootScope.queue = [];
-                            angular.forEach(items, function (item, key) {
-                                if (item.status != "skipped") {
-                                    $rootScope.queue.push(map(item));
-                                }
-                            });
-                            var next = $rootScope.queue[0];
-                            $scope.$apply(function () {
-                                $rootScope.playSong(false, next);
-                            });
-                            $rootScope.showQueue();
-                            notifications.updateMessage(items.length + ' Song(s) Added to Queue', true);
+        $scope.getPodcasts = function(refresh) {
+            if (globals.settings.Debug) {
+                console.log("LOAD PODCASTS");
+            }
+            $.ajax({
+                url: globals.BaseURL() + '/getPodcasts.view?' + globals.BaseParams(),
+                method: 'GET',
+                dataType: globals.settings.Protocol,
+                timeout: globals.settings.Timeout,
+                success: function(data) {
+                    if (data["subsonic-response"].podcasts.channel !== undefined) {
+                        var items = [];
+                        if (data["subsonic-response"].podcasts.channel.length > 0) {
+                            items = data["subsonic-response"].podcasts.channel;
                         } else {
-                            $scope.album = [];
-                            $rootScope.song = [];
-                            angular.forEach(items, function (item, key) {
-                                if (item.status != "skipped") {
-                                    $rootScope.song.push(map(item));
-                                }
-                            });
-                            $scope.$apply();
+                            items[0] = data["subsonic-response"].podcasts.channel;
+                        }
+                        $scope.podcasts = items;
+                        $scope.$apply();
+                    }
+                }
+            });
+        };
+
+        $scope.getPodcast = function(id, action) {
+            $scope.selectedPodcast = id;
+            var map = function(data) {
+                var song = data;
+                var url, track, rating, starred, contenttype, suffix, description;
+                var specs = '',
+                    coverartthumb = '',
+                    coverartfull = '';
+                if (typeof song.coverArt != 'undefined') {
+                    coverartthumb = globals.BaseURL() + '/getCoverArt.view?' + globals.BaseParams() + '&size=60&id=' + song.coverArt;
+                    coverartfull = globals.BaseURL() + '/getCoverArt.view?' + globals.BaseParams() + '&id=' + song.coverArt;
+                }
+                if (typeof song.description == 'undefined') {
+                    description = '';
+                } else {
+                    description = song.description;
+                }
+                if (typeof song.track == 'undefined') {
+                    track = '&nbsp;';
+                } else {
+                    track = song.track;
+                }
+                if (typeof song.starred !== 'undefined') {
+                    starred = true;
+                } else {
+                    starred = false;
+                }
+                if (song.bitRate !== undefined) {
+                    specs += song.bitRate + ' Kbps';
+                }
+                if (song.transcodedSuffix !== undefined) {
+                    specs += ', transcoding:' + song.suffix + ' > ' + song.transcodedSuffix;
+                } else {
+                    specs += ', ' + song.suffix;
+                }
+                if (song.transcodedSuffix !== undefined) {
+                    suffix = song.transcodedSuffix;
+                } else {
+                    suffix = song.suffix;
+                }
+                if (suffix == 'ogg') {
+                    suffix = 'oga';
+                }
+                var salt = Math.floor(Math.random() * 100000);
+                url = globals.BaseURL() + '/stream.view?' + globals.BaseParams() + '&id=' + song.streamId + '&salt=' + salt;
+                return new model.Song(song.streamId, song.parent, track, song.title, song.artist, song.artistId, song.album, song.albumId, coverartthumb, coverartfull, song.duration, song.userRating, starred, suffix, specs, url, 0, description);
+            };
+
+            $.ajax({
+                url: globals.BaseURL() + '/getPodcasts.view?' + globals.BaseParams(),
+                method: 'GET',
+                dataType: globals.settings.Protocol,
+                timeout: globals.settings.Timeout,
+                success: function(data) {
+                    if (data["subsonic-response"].podcasts.channel !== undefined) {
+                        var podcasts = [];
+                        if (data["subsonic-response"].podcasts.channel.length > 0) {
+                            podcasts = data["subsonic-response"].podcasts.channel;
+                        } else {
+                            podcasts[0] = data["subsonic-response"].podcasts.channel;
+                        }
+                        var items = [];
+                        $.each(podcasts, function(i, item) {
+                            if (item.id == id) {
+                                items = item.episode;
+                            }
+                        });
+
+                        if (typeof items != 'undefined') {
+                            if (action == 'add') {
+                                angular.forEach(items, function(item, key) {
+                                    if (item.status != "skipped") {
+                                        $rootScope.queue.push(map(item));
+                                    }
+                                });
+                                $scope.$apply();
+                                $rootScope.showQueue();
+                                notifications.updateMessage(items.length + ' Song(s) Added to Queue', true);
+                            } else if (action == 'play') {
+                                $rootScope.queue = [];
+                                angular.forEach(items, function(item, key) {
+                                    if (item.status != "skipped") {
+                                        $rootScope.queue.push(map(item));
+                                    }
+                                });
+                                var next = $rootScope.queue[0];
+                                $scope.$apply(function() {
+                                    $rootScope.playSong(false, next);
+                                });
+                                $rootScope.showQueue();
+                                notifications.updateMessage(items.length + ' Song(s) Added to Queue', true);
+                            } else {
+                                $scope.album = [];
+                                $rootScope.song = [];
+                                angular.forEach(items, function(item, key) {
+                                    if (item.status != "skipped") {
+                                        $rootScope.song.push(map(item));
+                                    }
+                                });
+                                $scope.$apply();
+                            }
                         }
                     }
                 }
-            }
-        });
-    }
+            });
+        };
 
-    /* Launch on Startup */
-    $scope.getPodcasts();
-    /* End Startup */
-});
+        /* Launch on Startup */
+        $scope.getPodcasts();
+        /* End Startup */
+    });
--- js/controllers/settings.js
+++ js/controllers/settings.js
@@ -1,83 +1,107 @@
 ﻿JamStash.controller('SettingsCtrl',
-function SettingsCtrl($scope, $routeParams, $location, utils, globals, json, notifications) {
-    $scope.settings = globals.settings;
-    $scope.Timeouts = [
-        { id: 10000, name: 10 },
-        { id: 20000, name: 20 },
-        { id: 30000, name: 30 },
-        { id: 40000, name: 40 },
-        { id: 50000, name: 50 },
-        { id: 60000, name: 60 },
-        { id: 90000, name: 90 },
-        { id: 120000, name: 120 }
-    ];
-    $scope.Protocols = ["json", "jsonp"];
-    $scope.Themes = ["Default", "Dark"];
-    $scope.SearchTypes = globals.SearchTypes;
-    $scope.Layouts = globals.Layouts;
+    function SettingsCtrl($scope, $routeParams, $location, utils, globals, json, notifications) {
+        $scope.settings = globals.settings;
 
-    $scope.$watch('settings.HideAZ', function () {
-        if (globals.settings.HideAZ) {
-            $('#AZIndex').hide();
-        } else {
-            $('#AZIndex').show();
-        }
-    });
-    $scope.reset = function () {
-        utils.setValue('Settings', null, true);
-        $scope.loadSettings();
-    }
-    $scope.save = function () {
-        if ($scope.settings.Password != '' && globals.settings.Password.substring(0, 4) != 'enc:') { $scope.settings.Password = 'enc:' + utils.HexEncode($scope.settings.Password); }
-        if (globals.settings.NotificationSong) {
-            notifications.requestPermissionIfRequired();
-            if (!notifications.hasNotificationPermission()) {
-                alert('HTML5 Notifications are not available for your current browser, Sorry :(');
+        $scope.Timeouts = [{
+            id: 10000,
+            name: 10
+        }, {
+            id: 20000,
+            name: 20
+        }, {
+            id: 30000,
+            name: 30
+        }, {
+            id: 40000,
+            name: 40
+        }, {
+            id: 50000,
+            name: 50
+        }, {
+            id: 60000,
+            name: 60
+        }, {
+            id: 90000,
+            name: 90
+        }, {
+            id: 120000,
+            name: 120
+        }];
+
+        $scope.Protocols = ["json", "jsonp"];
+        $scope.Themes = ["Default", "Dark"];
+        $scope.SearchTypes = globals.SearchTypes;
+        $scope.Layouts = globals.Layouts;
+
+        $scope.$watch('settings.HideAZ', function() {
+            if (globals.settings.HideAZ) {
+                $('#AZIndex').hide();
+            } else {
+                $('#AZIndex').show();
             }
-        }
-        if (globals.settings.NotificationNowPlaying) {
-            notifications.requestPermissionIfRequired();
-            if (!notifications.hasNotificationPermission()) {
-                alert('HTML5 Notifications are not available for your current browser, Sorry :(');
-            }
-        }
-        if (globals.settings.SaveTrackPosition) {
-            //saveTrackPosition();
-        } else {
-            //deleteCurrentPlaylist();
-        }
-        if (globals.settings.Theme) {
-            utils.switchTheme(globals.settings.Theme);
-        }
-        utils.setValue('Settings', $scope.settings, true);
-        notifications.updateMessage('Settings Updated!', true);
-        $scope.loadSettings();
-        if (globals.settings.Server != '' && globals.settings.Username != '' && globals.settings.Password != '') {
-            $scope.ping();
-        }
-    };
-    json.getChangeLog(function (data) {
-        $scope.changeLog = data.slice(0, 10);
-    });
-    $scope.changeLogShowMore = function () {
-        json.getChangeLog(function (data) {
-            $scope.changeLog = data;
         });
-    }
-    $scope.setupDemo = function () {
-        var Username = "android-guest";
-        var Password = "guest";
-        var Server = "http://subsonic.org/demo";
-        var Tab = "tabLibrary";
-        if (utils.confirmDelete("Do you want to connect to the Subsonic Demo server?")) {
-            globals.settings.Username = Username;
-            globals.settings.Password = Password;
-            globals.settings.Server = Server;
-            //$scope.save();
-            $location.url('/library');
-        }
-    }
 
-    /* Load on Startup */
-    /* End Startup */
-});
+        $scope.reset = function() {
+            utils.setValue('Settings', null, true);
+            $scope.loadSettings();
+        };
+
+        $scope.save = function() {
+            if ($scope.settings.Password !== '' && globals.settings.Password.substring(0, 4) != 'enc:') {
+                $scope.settings.Password = 'enc:' + utils.HexEncode($scope.settings.Password);
+            }
+            if (globals.settings.NotificationSong) {
+                notifications.requestPermissionIfRequired();
+                if (!notifications.hasNotificationPermission()) {
+                    alert('HTML5 Notifications are not available for your current browser, Sorry :(');
+                }
+            }
+            if (globals.settings.NotificationNowPlaying) {
+                notifications.requestPermissionIfRequired();
+                if (!notifications.hasNotificationPermission()) {
+                    alert('HTML5 Notifications are not available for your current browser, Sorry :(');
+                }
+            }
+            if (globals.settings.SaveTrackPosition) {
+                //saveTrackPosition();
+            } else {
+                //deleteCurrentPlaylist();
+            }
+            if (globals.settings.Theme) {
+                utils.switchTheme(globals.settings.Theme);
+            }
+            utils.setValue('Settings', $scope.settings, true);
+            notifications.updateMessage('Settings Updated!', true);
+            $scope.loadSettings();
+            if (globals.settings.Server !== '' && globals.settings.Username !== '' && globals.settings.Password !== '') {
+                $scope.ping();
+            }
+        };
+
+        json.getChangeLog(function(data) {
+            $scope.changeLog = data.slice(0, 10);
+        });
+
+        $scope.changeLogShowMore = function() {
+            json.getChangeLog(function(data) {
+                $scope.changeLog = data;
+            });
+        };
+
+        $scope.setupDemo = function() {
+            var Username = "android-guest";
+            var Password = "guest";
+            var Server = "http://subsonic.org/demo";
+            var Tab = "tabLibrary";
+            if (utils.confirmDelete("Do you want to connect to the Subsonic Demo server?")) {
+                globals.settings.Username = Username;
+                globals.settings.Password = Password;
+                globals.settings.Server = Server;
+                //$scope.save();
+                $location.url('/library');
+            }
+        };
+
+        /* Load on Startup */
+        /* End Startup */
+    });
