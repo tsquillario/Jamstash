608d4f98e205a733a79866d1ec92ccdd5692fe49
Hyzual <hyzual@gmail.com>
2014-11-22 18:58:15 +0100

    Extracts the player into its own files.
    Creates a player directive, controller and service.
    Moves the HTML from inside index.html to its own player.html file.
    
    Conflicts:
    	app/index.html

---
+++ app/player/player.js
@@ -0,0 +1,12 @@
+/**
+* jamstash.player Module
+*
+* Control the HTML5 player through jplayer.js
+*/
+angular.module('jamstash.player', ['jamstash.player.service', 'jamstash.player.directive'])
+
+.controller('PlayerCtrl', ['$scope', function($scope){
+	'use strict';
+
+
+}]);
