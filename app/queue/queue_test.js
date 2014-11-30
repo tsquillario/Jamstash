27197fb05ed1dfc44bfc35b57f0eae50b3cec4eb
Hyzual <hyzual@gmail.com>
2014-11-30 19:25:23 +0100

    Fixes the queue test

--- app/queue/queue_test.js
+++ app/queue/queue_test.js
@@ -1,30 +1,33 @@
 describe("Queue controller", function() {
     'use strict';
 
-    var player, scope;
+    var player, $rootScope, scope, globals;
 
     beforeEach(function() {
         module('jamstash.queue.ctrl');
 
-        inject(function ($controller, $rootScope, _player_) {
+        inject(function ($controller, _$rootScope_, _globals_, _player_) {
+            $rootScope = _$rootScope_;
             scope = $rootScope.$new();
+            globals = _globals_;
             player = _player_;
 
             // Mock the functions of the services
             spyOn(player, "playSong").and.stub();
 
-            $controller('PlayerCtrl', {
+            $controller('QueueCtrl', {
+                $rootScope: $rootScope,
                 $scope: scope,
+                globals: globals,
                 player: player
             });
         });
+    });
+    it("When I call playSong, it calls playSong in the player service", function() {
+        var fakeSong = {"id": 3174};
 
-        it("When I call playSong, it calls playSong in the player service", function() {
-            var fakeSong = {"id": 3174};
+        scope.playSong(true, fakeSong);
 
-            scope.playSong(true, fakeSong);
-
-            expect(player.playSong).toHaveBeenCalledWith(true, fakeSong);
-        });
+        expect(player.playSong).toHaveBeenCalledWith(true, fakeSong);
     });
 });
