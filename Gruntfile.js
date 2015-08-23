dd252e9424951100997dd1bbef2d1370bb4f1fe8
Trevor Squillario <tsquillario@gmail.com>
2015-08-23 11:44:01 -0400

    4.6.2 fix songdetails_controls loop icon spacing and uglifyjs in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -182,7 +182,7 @@ module.exports = function (grunt) {
         flow: {
           html: {
             steps: {
-              js: ['concat', 'uglifyjs'],
+              js: ['concat', 'uglify'],
               css: ['cssmin']
             },
             post: {}
